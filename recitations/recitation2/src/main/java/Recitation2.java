b5cb57f02fb25d4fb1578b2048461032e8ad6cc3
Ben Fu <Ben Fu>
2017-09-12 13:18:22 -0500

    Add reading from stdin

--- recitations/recitation2/src/main/java/Recitation2.java
+++ recitations/recitation2/src/main/java/Recitation2.java
@@ -1,9 +1,34 @@
 
-import org.slf4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Recitation2 {
     public static void main(String[] args) {
         System.out.println("Hello World\n");
+
+        Recitation2 program = new Recitation2();
+
+        try
+        (
+            InputStreamReader reader = new InputStreamReader(System.in);
+            BufferedReader bufferedReader = new BufferedReader(reader)
+        )
+        {
+            Integer num = Integer.parseInt(bufferedReader.readLine());
+            System.out.println("the sum of the digits in " + num + " is " + program.sumOfDigits(num));
+
+        }
+
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        String str = scanner.nextLine();
+//        System.out.println(str);
     }
 
     public int sumOfDigits(int n) {
