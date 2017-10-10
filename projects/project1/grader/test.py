fd2aae759f27aefa5b395e6a0b0e2e774bce2a87
Ben Fu <ben_fu1@yahoo.com>
2017-10-10 11:52:09 -0500

    Add project 2 pdf

--- projects/project1/grader/test.py
+++
@@ -1,8 +0,0 @@
-
-class Test:
-    def __init__(self, name, input_filename, output_filename, solution_filename):
-        self.name = name
-        self.input_filename = input_filename
-        self.output_filename = output_filename
-        self.solution_filename = solution_filename
-        
