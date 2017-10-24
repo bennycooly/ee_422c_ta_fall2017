b4227363a83f6cac9a4a06dbb8db75132e5227d2
Ben Fu <ben_fu1@yahoo.com>
2017-10-24 03:07:58 -0500

    Update exception code

--- lectures/exceptions/ExceptionDemo.java
+++ lectures/exceptions/ExceptionDemo.java
@@ -1,6 +1,10 @@
 
 import java.lang.ArithmeticException;
 
+/**
+ * Helpful links:
+ * https://www.programcreek.com/2009/02/diagram-for-hierarchy-of-exception-classes/
+ */
 public class ExceptionDemo {
 
     public static void main(String[] args) {
@@ -21,13 +25,16 @@ public class ExceptionDemo {
 
 
     /**
-     * Example of a checked exception.
+     * Example of an unchecked exception.
      */
     public int divide(int a, int b) throws ArithmeticException {
         return a/b;
     }
 
 
+    /**
+     * Example of a checked exception.
+     */
     public boolean checkGame(String state) throws GameException {
         if (state.equals("illegal")) {
             throw new GameException();
